d78fad3866aabcbaaee1f5042ce43512abe6b582
xuwei <xuwei@mygit.com>
2016-01-04 15:04:25 +0800

    4-1 session and flash

--- app/hello.py
+++ app/hello.py
@@ -1,7 +1,7 @@
 #-*- coding:utf-8 -*-
 import os
 import sys
-from flask import Flask, render_template
+from flask import Flask, render_template, session, redirect, url_for, flash
 from flask.ext.script import Manager
 from flask.ext.bootstrap import Bootstrap
 from flask.ext.sqlalchemy import SQLAlchemy
@@ -19,13 +19,14 @@ app.config.from_object('config')
 
 @app.route('/', methods=['GET','POST'])
 def index():
-    name = None
     form = NameForm()
     if form.validate_on_submit():
-        name = form.name.data
-        print name
-        form.name.data = ''
-    return render_template('index.html', form=form, name=name)
+       old_name = session.get('name')
+       if old_name is not None and old_name != form.name.data:
+           flash('Looks like you have changed your name!')
+       session['name'] = form.name.data
+       return redirect(url_for('index'))
+    return render_template('index.html', form=form, name=session.get('name'))
 
 @app.errorhandler(404)
 def page_not_found(e):
