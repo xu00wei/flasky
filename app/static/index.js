550b7c598dc8f2eb62f027e28ed145c025bc4f60
xuwei <xuwei@mygit.com>
2016-03-20 23:44:05 +0800

    新增响应式首页

--- app/static/index.js
+++ app/static/index.js
@@ -13,6 +13,8 @@
 //
 $(document).ready(function(){
     var need_remove_circle = true;
+    var window_width = $(window).width();
+
     $(".nav-mobile").click(function(){
         if(need_remove_circle){
             $(".nav-mobile").removeClass("glyphicon-menu-hamburger");
@@ -25,5 +27,34 @@ $(document).ready(function(){
             need_remove_circle = true;
         }
     });
+
+    $(window).resize(function(){
+        now_window_width = $(window).width();
+        if(now_window_width <= 768 && window_width > 768 && $('#title').attr("class") != "full-title"){
+            $("#title").addClass("mobile-left-title").removeClass("left-title");
+            window_width = now_window_width;
+        }
+        else if( now_window_width> 768 && window_width <= 768 && $('#title').attr("class") != "full-title"){
+            $('#title').addClass("left-title").removeClass("mobile-left-title");
+            window_width = now_window_width;
+        }
+    });
+
+    $(".go-index").click(function(){
+        if( $(window).width() >= 768 && $('#title').attr("class") == "full-title"){
+            $("#title").animate({ width:'25%'},'normal',function(){
+                $("#title").addClass("left-title").removeClass("full-title").removeAttr("style");
+            });
+            $('.index-content').css("display","block");
+        }
+        else{
+            $("#title").animate({ height:'20%' }, 'normal', function(){
+                 $("#title").addClass("mobile-left-title").removeClass("full-title").removeAttr("style");
+            });
+            $('.index-content').css("display","block");
+
+        }
+    });
 })
+
 
