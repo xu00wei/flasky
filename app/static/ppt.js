5a01bffd2017f282a112345716f95f0b8f458716
xuwei <xuwei@mygit.com>
2016-03-22 15:33:53 +0800

    ppt布局初步

--- app/static/ppt.js
+++ app/static/ppt.js
@@ -1,12 +1,28 @@
+
 $(document).ready(function(){
-    var now_offset_top = $(window).offset().top;
     var window_height = $(window).height();
 
-    $(window).scroll(function(){
-        if(now_offset_top < window_height){
+    $('.body').scroll(function(){
+        // alert(window_height + '...'+$('#about').offset().top+"..."+$("#frame").offset().top);
+        var active_li = $('#navbar-vertical ul .active');
+        var div_id = active_li.children().attr("href");
+        var now_div = $(div_id);
+        //alert(div_id+":"+now_div.offset().top);
 
+        if ( now_div.offset().top > 0 ){
+                       active_li.removeClass("active").animate( {} ,'speed', function(){
+                active_li.prev().addClass("active").animate( {} , 'speed', function(){
+                    active_li.prev().children()[0].click();
+                });
+            });
         }
-    })
-
+        else if( now_div.offset().top < 0 ){
+                       active_li.removeClass("active").animate( {} ,'speed', function(){
+                active_li.next().addClass("active").animate( {} , 'speed', function(){
+                    active_li.next().children()[0].click();
+                });
+            });
+        }
+    });
 
 })
