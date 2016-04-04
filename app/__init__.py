28a30312627182e098193b1ce6a5546a8c37118c
xuwei <xuwei@mygit.com>
2016-04-04 20:36:51 +0800

    some migrate error

--- app/__init__.py
+++ app/__init__.py
@@ -42,4 +42,6 @@ def create_app(config_name):
     from .auth import auth as auth_blueprint
     app.register_blueprint(auth_blueprint, url_prefix='/auth')
 
+    from models import *
+
     return app
